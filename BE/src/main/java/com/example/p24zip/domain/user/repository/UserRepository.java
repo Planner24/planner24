535013b164cd175ac9b741ecc6c9e6fd8ce43a4e
cocobabb <cocobab05108@gmail.com>
2025-03-16 00:15:38 +0900

    [P24-12] feat: 회원가입 기능 완성

--- BE/src/main/java/com/example/p24zip/domain/user/repository/UserRepository.java
+++ BE/src/main/java/com/example/p24zip/domain/user/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.example.p24zip.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    
+    boolean existsByUsername(String userName);
 }
